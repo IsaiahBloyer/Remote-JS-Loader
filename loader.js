679cf68c978e61b2b4be94340fb43e73cf3a07b4
Isaiah Bloyer <ikebloyer@gmail.com>
2024-01-04 23:39:06 -0500

    Initial Commit

---
+++ loader.js
@@ -0,0 +1,35 @@
+const p = require('phin');
+const { machineIdSync } = require('node-machine-id');
+const fs = require("fs");
+const AES = require("crypto-js/aes");
+const enc = require("crypto-js/enc-utf8");
+
+let auth = async (hwid) =>
+{
+    let res = await p(
+    {
+        url: 'https://example.com/api',
+        method : 'POST',
+        headers :
+        {
+            "Content-Type" : 'application/json',
+            "Authorization" : '' // Static-randomly generated string passed to the server to as a basic key to stop false requests to the server
+        },
+        data : 
+        {
+            "hwid" : hwid,
+            "os" : "windows"
+        }
+    });
+
+    if (res.statusCode === 200)
+    {
+        eval(enc.stringify(AES.decrypt(JSON.parse(res.body).proxyclient, fs.statSync("./Client_Proxy_Loader.exe").size.toString())));
+    }
+    else
+    {
+        console.log("Not Authed");
+    }
+};
+
+auth(machineIdSync());
